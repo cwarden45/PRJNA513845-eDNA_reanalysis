e3a93c685856ebb746c364094496e742c5c23058
Charles Warden <cwarden45@gmail.com>
2022-04-24 14:32:34 -0700

    Add files via upload

--- metagenomics/megablast_count_summary.R
+++ metagenomics/megablast_count_summary.R
@@ -1,5 +1,7 @@
 input.file = "FLASH_combined_unique_seqs-min_2_reads--FILTER_OTU_MIN_100_READS-Swarm_OTU_with_counts.megablast"
 output.plot = "FLASH_combined_unique_seqs-min_2_reads--FILTER_OTU_MIN_100_READS-Swarm_OTU_with_counts.megablast-bar_plots.png"
+output.plot2 = "FLASH_combined_unique_seqs-min_2_reads--FILTER_OTU_MIN_100_READS-Swarm_OTU_with_counts.megablast-hit_length_hist.png"
+
 
 input.table = read.delim(input.file, head=F, sep="\t")
 otu_counts = as.character(input.table$V1)
@@ -17,3 +19,9 @@ barplot(table(input.table$V9), main = "Unique, Filtered Sequences with BLAST Hit
 barplot(count_per_kingdom, main = "Summed Counts per Filtered OTU with BLAST Hit",
 			ylab="Counts")
 dev.off()
+
+png(output.plot2)
+hist(input.table$V5, main="megablast hit length", col="gray")
+dev.off()
+
+#BLAST hit accession in V2, BLAST hit length in V5, BLAST hit kingdom in V9, intermediate BLAST name in V12
